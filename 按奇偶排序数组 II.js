3892987eeec4113df74c114e70899515adcd2cae
zhangxing <zhangxing@kuaishou.com>
2022-03-31 18:47:18 +0800

    feat: 新增按奇偶排序数组

--- 按奇偶排序数组 II.js
+++ 按奇偶排序数组 II.js
@@ -12,6 +12,53 @@
 // 示例 2：
 // 输入：nums = [2,3]
 // 输出：[2,3]
+
+/**
+ * 时间复杂度o(N), 空间复杂度o(N)太高; (不考虑输出数组的空间占用)
+ * @param {*} nums
+ * @returns
+ */
 var sortArrayByParityII = function (nums) {
-  // TODO
+  let odds = [];
+  let evens = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      evens.push(nums[i]);
+    } else {
+      odds.push(nums[i]);
+    }
+  }
+
+  let res = new Array(nums.length).fill(0);
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      res[i] = evens.shift();
+    } else {
+      res[i] = odds.shift();
+    }
+  }
+  return res;
 };
+console.log(sortArrayByParityII([4, 2, 5, 7]));
+
+/**
+ * 时间复杂度o(N), 空间复杂度o(1); (不考虑输出数组的空间占用)
+ * @param {*} nums
+ * @returns
+ */
+var sortArrayByParityII2 = function (nums) {
+  let res = new Array(nums.length).fill(0);
+  let i = 0;
+  let j = 1;
+  nums.forEach((item) => {
+    if (item % 2 === 0) {
+      res[i] = item;
+      i += 2;
+    } else {
+      res[j] = item;
+      j += 2;
+    }
+  });
+  return res;
+};
+console.log(sortArrayByParityII2([4, 2, 5, 7]));
